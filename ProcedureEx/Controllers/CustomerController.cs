d82da0533d8fe0c12cb89856548d544ff2fdaaa4
Simo <Simo@SIMO>
2022-08-23 00:51:21 -0400

    Add project files.

---
+++ ProcedureEx/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProcedureEx.Models;
+
+namespace ProcedureEx.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private NorthwindContext Context { get; }
+        public CustomerController(NorthwindContext _context)
+        {
+            this.Context = _context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Customer> customers = this.Context.Customers.ToList();
+            return View(customers);
+        }
+
+        [HttpPost]
+        public IActionResult Index(string customerName)
+        {
+            List<Customer> customers = this.Context.SearchCustomers(customerName).ToList();
+            return View(customers);
+        }
+    }
+}
---
+++ ProcedureEx/Models/NorthwindContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Query;
+namespace ProcedureEx.Models
+{
+    public partial class NorthwindContext : DbContext
+    {
+        public NorthwindContext()
+        {
+        }
+
+        public NorthwindContext(DbContextOptions<NorthwindContext> options)
+            : base(options)
+        {
+        }
+
+      
+        public virtual DbSet<Customer> Customers { get; set; } = null!;
+
+
+        public IQueryable<Customer> SearchCustomers(string contactName)
+        {
+            SqlParameter pContactName = new SqlParameter("@ContactName", contactName);
+            return this.Customers.FromSqlRaw("EXECUTE Customers_SearchCustomers @ContactName", pContactName);
+        }
+
+
+
+    }
+}
